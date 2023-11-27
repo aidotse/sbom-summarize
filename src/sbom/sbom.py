75ef483fb0c87ad3a32f3cc343a027b5fd50a9d2
Max Petersson <max.petersson@ai.se>
2023-11-27 10:27:05 +0100

    Check if the json is an possible SBOM, if not trow a warning. Also some updates on the printout.

--- src/sbom/sbom.py
+++ src/sbom/sbom.py
@@ -8,6 +8,8 @@ class Sbom:
     def __init__(self, sbom_path: str) -> None:
         self.sbom_path = Path(sbom_path)
         self.sbom = json.load(self.sbom_path.open())
+        if list(self.sbom.keys())[0] != 'SPDXID':
+            print('---\nIs this a valid SBOM?')
 
     def pp_sbom(self) -> None:
         print(json. dumps(self.sbom, indent=4))
@@ -16,15 +18,30 @@ class Sbom:
         licenses = []
         no_licensed_packages = []
         for package in self.sbom['packages']:
-            if('licenseConcluded' in package):
-                licenses.append(package['licenseConcluded'])
-            else:
-                no_licensed_packages.append(package['name'])
+            if package['SPDXID'] not in self.sbom['documentDescribes']:
+                if('licenseConcluded' in package):
+                    licenses.append(package['licenseConcluded'])
+                else:
+                    no_licensed_packages.append(package['name'])
 
         licenses_counter = Counter(licenses)
 
-        for license_type in licenses_counter:
-            print(license_type,':',licenses_counter[license_type])
+        print('---\nSummarize\nSBOM-file: ', self.sbom_path.name,'\n')
 
-        print('Packages without licenses:\n',no_licensed_packages)
+        if len(licenses_counter) > 0: 
+            print('License count:')
+            for license_type in licenses_counter:
+                print(license_type,':',licenses_counter[license_type])
+        else:
+            print('No packages with licenses found.')
+        print('\n')
+
+        if len(no_licensed_packages) > 0:
+            print('Packages without licenses:')
+            for no_lic_pack in no_licensed_packages:
+                print(no_lic_pack)
+        else:
+            print('No packages without license found.')
+
+        print('---')
 
